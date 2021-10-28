e8aeaefd3663ec5f9d6e1f3b819453962dd45c80
vikasbhalla <vikas.bhalla@nagarro.com>
2021-10-28 18:34:48 +0530

    updated service class

---
+++ src/main/java/com/nagarro/assignment09/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nagarro.assignment09.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Assignment09RestApiApplication.class);
+	}
+
+}
